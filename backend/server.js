eef1e001cdf5a25d4a3510d6d89e2f98100597ee
caue96 <cauemateus@hotmail.com>
2021-08-27 03:17:55 -0300

    Now everything is working fine with docker and docker-compose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
 const app = express();
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.json({status: true});
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT);
 console.log(`Running on http://${HOST}:${PORT}`);
