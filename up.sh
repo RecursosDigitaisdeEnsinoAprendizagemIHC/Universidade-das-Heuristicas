8b4e923d1455eb10cee1046e0e9ca1c644a6b2fb
MoacirMSJ <moacirmascarenha86@gmail.com>
2022-06-05 22:44:28 -0300

    update up.sh to run all

--- up.sh
+++ up.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+print_before_command() {
+echo ""
+echo "######################################"
+}
+
 clear;
 
+print_before_command
+echo "Install back dependency"
+(cd back && npm install)
+
+print_before_command
+echo "Install front dependency"
+(cd front && npm install)
+
+print_before_command
+echo "Run docker-compose down"
 docker-compose down;
+
+print_before_command
+echo "Run docker-compose up"
 docker-compose up -d;
 
-cd back || exit;
+print_before_command
+echo "Run migrations"
+(cd back && npm run typeorm-sync && npm run typeorm-migration)
 
-npm run dev;
-
-# cd ../front || exit;
-
-# npm run dev;
+print_before_command
+echo "Run front"
+(cd front && npm run dev)
 
